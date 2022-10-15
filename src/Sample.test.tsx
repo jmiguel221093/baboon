be523cb3d6b00741dbe497e5af16c17e97c26d6b
Miguel González <jmiguel@Mac-mini-de-Miguel.local>
2022-10-15 00:33:02 -0500

    test

---
+++ src/Sample.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+
+test("renders learn react link", () => {
+	render(
+		<div>
+			<a href="#">learn react</a>
+		</div>
+	);
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
+});
