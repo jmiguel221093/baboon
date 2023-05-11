65df890d7bfee5ed02f02436aa4b8c8180064e58
Miguel González <jmiguel@Mac-mini-de-Miguel.local>
2023-05-11 16:59:40 -0600

    Any explicity types disabled for linting

--- src/utils/common/isValidIcon.ts
+++ src/utils/common/isValidIcon.ts
@@ -1,5 +1,6 @@
 import type { IconSourceType } from "../../components";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const isValidIcon = (icon: any): icon is IconSourceType =>
 	typeof icon === "string" || typeof icon === "function";
 
